4689e860d85f9cfcbd7171d4a51ce63267cad5c5
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-11-19 14:33:22 -0800

    REPLACEMENT.sh [V2]
    Version 2 of the replacement script file for this directory set.

--- EUShell/Operator/Replacement/REPLACEMENT.sh
+++ EUShell/Operator/Replacement/REPLACEMENT.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 # Replace the US Dollar sign with the Euro sign
 '$' -> '€'
-# File version: 1 (2022, Saturday, November 19th at 2:21 pm PST)
+# File info
+# File type: BASH shell source file (*.sh)
+# File version: 2 (2022, Saturday, November 19th at 2:33 pm PST)
+# Line count (including blank lines and compiler line): 8
